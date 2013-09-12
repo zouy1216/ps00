edd42e6cd8260bc8b433aafddc7b9ca79095d2e5
zouy1216 <Ying@Yings-MacBook-Pro.local>
2013-09-12 18:20:07 -0400

    version 3.0

---
+++ set00:12.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname set00:12) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(make-posn 5 3)
+(posn? 5)
+(posn? true)
+(posn? (make-posn 2 1))
+(posn-x (make-posn 8 5))
+(posn-y (make-posn 42 15))
